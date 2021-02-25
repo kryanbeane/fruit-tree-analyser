e22caa34307de7a326b708af3cd414620ffab1be
Bryan Keane <20089166@mail.wit.ie>
2021-02-25 12:01:50 +0000

    Added some small tweaks, greyscale conversion now compares HSB of selected pixel with every pixel in the image, if hsb is within range of selected pixel's HSB it converts the matches to black and rest to white, needs tweaking but primitive version is working! Also added HashMap.java.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,92 +1,130 @@
 package sample;
 import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
 import javafx.scene.image.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
+
 import java.io.File;
+import java.io.FileInputStream;
 
 public class Controller {
-    Double red, green, blue;
-    Image image;
+    Color selectedColor, currentColor;
+    double selectedHue, currentHue, selectedSaturation, currentSaturation, selectedBrightness, currentBrightness;
+    Image image, greyImage;
     int width, height;
-    PixelReader pr;
+    PixelReader originalPR, greyPR;
     PixelWriter originalPW, greyPW;
     WritableImage originalWI, greyWI;
     @FXML BorderPane homeTab, greyscaleTab, fruitRecogTab, settingsTab;
-    @FXML ImageView originalImageView;
+    @FXML ImageView originalImageView, greyScaleImageView;
+    @FXML Slider hueSlider, saturationSlider, brightnessSlider;
 
-    public void openImage() {
-        FileChooser fc = new FileChooser();
-        fc.setTitle("Open Image");
-        // Exempts file non-JPG and non-PNG files from file chooser dialogue box
-        FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
-        FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
-        fc.getExtensionFilters().addAll (extFilterPNG, extFilterJPG);
-        // Opens dialogue
-        File file = fc.showOpenDialog(null);
-
-        try {
-            if (file != null) {
-                Image image = new Image(file.toURI().toString());
-                displayImage(image);
-            }
-        } catch(NullPointerException ignored) {
-            System.out.println("Didn't work");
-        }
+    public boolean compareHue(double firstHue, double secondHue) {
+        return (Math.abs(firstHue-secondHue) < 40);
     }
 
-    public void displayImage(Image chosenFile) {
-        image = chosenFile;
+    public boolean compareSaturation(double firstSaturation, double secondSaturation) {
+        return (Math.abs(firstSaturation-secondSaturation) < .3);
+    }
+
+    public boolean compareBrightness(double firstBrightness, double secondBrightness) {
+        return (Math.abs(firstBrightness-secondBrightness) < .3);
+    }
+
+    public Image greyScaleConversion() {
+        greyImage=image;
+        greyPR=greyImage.getPixelReader();
+        greyWI = new WritableImage(greyPR, width, height);
+        greyPW = greyWI.getPixelWriter();
+        greyPR=greyWI.getPixelReader();
+        selectedHue=selectedColor.getHue();
+        selectedSaturation=selectedColor.getSaturation();
+        selectedBrightness=selectedColor.getBrightness();
+
+        for(int x=1; x<width; x++) {
+            for(int y=1; y<height; y++) {
+                currentColor=greyPR.getColor(x, y);
+                setCurrentHSB();
+
+                if(compareHue(currentHue, selectedHue) && compareSaturation(currentSaturation, selectedSaturation) && compareBrightness(currentBrightness, selectedBrightness)) {
+                    greyPW.setColor(x, y, Color.valueOf("#000000"));
+                } else {
+                    greyPW.setColor(x, y, Color.valueOf("#ffffff"));
+                }
+            }
+        }
+        greyImage=greyWI;
+        return greyImage;
+    }
+
+    public Color getColourAtMouseR(javafx.scene.input.MouseEvent mouseEvent) {
+        return selectedColor = originalPR.getColor ((int) mouseEvent.getX(), (int) mouseEvent.getY());
+    }
+
+    public void openImage() {
+        try {
+            FileChooser fc = new FileChooser();
+            fc.setTitle("Open Image");
+            // Exempts file non-JPG and non-PNG files from file chooser dialogue box
+            FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
+            FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
+            fc.getExtensionFilters().addAll (extFilterPNG, extFilterJPG);
+            // Opens dialogue
+            File file = fc.showOpenDialog(null);
+
+            if (file != null) {
+                image = new Image(new FileInputStream(file), originalImageView.getFitWidth(), originalImageView.getFitHeight(), false, true);
+                displayImage();
+            }
+        } catch(Exception exception) {
+            System.out.println("Image load didn't work");
+        }
+    }
+    public void displayImage() {
         width = (int) image.getWidth();
         height = (int) image.getHeight();
-        pr=image.getPixelReader();
-        originalWI = new WritableImage(pr, width, height);
+        PixelReader originalPR=image.getPixelReader();
+        originalWI = new WritableImage(originalPR, width, height);
         originalPW = originalWI.getPixelWriter();
         originalImageView.setImage(image);
     }
-
-    public Image greyscaleConversion(Image originalImage) {
-        return null;
+    public void setCurrentHSB() {
+        currentHue=currentColor.getHue();
+        currentSaturation=currentColor.getSaturation();
+        currentBrightness=currentColor.getBrightness();
     }
-
+    public void setGreyscale() {
+        Image newGreyImage = greyScaleConversion();
+        greyScaleImageView.setImage(newGreyImage);
+    }
     public void homeButton() {
         homeTab.toFront();
         greyscaleTab.toBack();
         fruitRecogTab.toBack();
         settingsTab.toBack();
     }
-
     public void greyscaleButton() {
         greyscaleTab.toFront();
         homeTab.toBack();
         fruitRecogTab.toBack();
         settingsTab.toBack();
     }
-
     public void fruitRecogButton() {
         fruitRecogTab.toFront();
         homeTab.toBack();
         greyscaleTab.toBack();
         settingsTab.toBack();
     }
-
     public void settingsButton() {
         settingsTab.toFront();
         homeTab.toBack();
         greyscaleTab.toBack();
         fruitRecogTab.toBack();
     }
-
     public void exit(){
         System.exit(0);
     }
 
-    public void getColourAtMouseR(javafx.scene.input.MouseEvent mouseEvent) {
-        Color color = pr.getColor((int) mouseEvent.getX(), (int) mouseEvent.getY());
-        red=color.getRed()*255;
-        green=color.getGreen()*255;
-        blue=color.getBlue()*255;
-        System.out.println(red + " " + green + " " + blue);
-    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,6 +3,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -15,7 +16,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../resources/main.fxml"));
-        primaryStage.initStyle(StageStyle.UNDECORATED);
+        //primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root, 850, 650));
         primaryStage.setResizable(false);
         primaryStage.show();
